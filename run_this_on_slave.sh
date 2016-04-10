331189d425a2befec73fc0fc664b2595cb93bc12
root <root@master.puppet>
2016-04-10 03:02:04 -0400

    script edited

--- run_this_on_slave.sh
+++ run_this_on_slave.sh
@@ -1,3 +1,16 @@
-puppet agent -t
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m' 
+/bin/echo "------------------------"
+printf "%s\n" "${red}Deploying configuration${end}"
+/bin/echo "------------------------"
+puppet agent -t &&
+/bin/echo "------------------------"
+printf "%s\n" "${red}Deployment Completed${end}"
+/bin/echo "------------------------"
 facter check_java
 facter check_jenkins
