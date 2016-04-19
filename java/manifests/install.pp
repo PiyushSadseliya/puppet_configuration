45a8232ff09b7e5a6a74f138cbc26acb2a94c348
nilayve7 <nilay.ve7@gmail.com>
2016-04-19 21:50:42 +0530

    java code edited

--- java/manifests/install.pp
+++ java/manifests/install.pp
@@ -1,11 +1,32 @@
-class java::install($java_archive="jdk-8u77-linux-x64.tar.gz",
-$java_folder="jdk1.8.0_77")
-{
+class java::install{
 
 file { "/tmp/$java_archive" :
 ensure => "present",
 source => "puppet:///modules/java/$java_archive",
+require => Exec["java_check_link2"],
 }
+
+exec { 'java_check_folder':
+path    => ["/usr/bin", "/usr/sbin", "/bin"],
+command => "rm -rf /opt/${java_folder}",
+onlyif => "/usr/bin/test -d /opt/${java_folder}",
+}
+
+exec { 'java_check_link1':
+path    => ["/usr/bin", "/usr/sbin", "/bin"],
+command => "rm -f /usr/bin/java",
+onlyif => "/usr/bin/test -L /usr/bin/java",
+require => Exec["java_check_folder"],
+}
+
+
+exec { 'java_check_link2':
+path    => ["/usr/bin", "/usr/sbin", "/bin"],
+command => "rm -f /usr/bin/javac",
+onlyif => "/usr/bin/test -L /usr/bin/javac",
+require => Exec["java_check_link1"],
+}
+
 
 exec { 'extract_java':
 path    => ["/usr/bin", "/usr/sbin", "/bin"],
@@ -21,13 +42,13 @@ cwd => "/tmp",
 command => "mv -f $java_folder /opt/",
 }
 
-file_line { 'edit_bashrc':
-require => Exec["set_java"],
-path => '/root/.bashrc',
-line => 'export JAVA_HOME=/opt/jdk1.8.0_77
-export JRE_HOME=/opt/jdk1.8.0_77/jre
-export PATH=$PATH:/opt/jdk1.8.0_77/bin:/opt/jdk1.8.0_77/jre/bin',
-}
+#file_line { 'edit_bashrc':
+#require => Exec["set_java"],
+#path => '/root/.bashrc',
+#line => 'export JAVA_HOME=/opt/jdk1.8.0_77
+#export JRE_HOME=/opt/jdk1.8.0_77/jre
+#export PATH=$PATH:/opt/jdk1.8.0_77/bin:/opt/jdk1.8.0_77/jre/bin',
+#}
 
 exec { 'set_links1':
 path    => ["/usr/bin", "/usr/sbin", "/bin"],
@@ -57,3 +78,4 @@ command => "rm -rf $java_archive",
 
 
 }
+
